7a41a0db347f929ba3262e09e839a1790be7e215
Aspenkate3 <playinroblox247@gmail.com>
2025-04-28 00:30:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-// select the canvas and get
-// its 2d context
+const SPRITE_WIDTH = 13;
+const SPRITE_HEIGHT = 14;
+const BORDER_WIDTH = 1;
+const SPACING_WIDTH = 1;
+
+function spritePositionToImagePosition(row, col) {
+    return {
+        x: (
+            BORDER_WIDTH +
+            col * (SPACING_WIDTH + SPRITE_WIDTH)
+        ),
+        y: (
+            BORDER_WIDTH +
+            row * (SPACING_WIDTH + SPRITE_HEIGHT)
+        )
+    }
+}
+
 var canvas = document
             .querySelector('canvas');
 var context = canvas
               .getContext('2d');
 
 var spriteSheetURL = 'https://codehs.com/uploads/e4cfb06e001bd92cf41139928e88819a';
-// create a new image from the spritesheet
 var image = new Image();
 image.src = spriteSheetURL;
 image.crossOrigin = true;
-// once the spritesheet loads,
-// draw it on the canvas
+
+var position = spritePositionToImagePosition(1, 0);
+
 image.onload = function() {
     context.drawImage(
         image,
+        // LOOK!
+        // we use the position from 
+        // spritePositionToImagePosition
+        // to start at an offset
+        // into the spritesheet!
+        position.x,
+        position.y,
+        SPRITE_WIDTH,
+        SPRITE_HEIGHT,
         0,
         0,
-        image.width,
-        image.height,
-        0,
-        0,
-        canvas.width,
-        canvas.height
+        SPRITE_WIDTH,
+        SPRITE_HEIGHT
     );
 };
